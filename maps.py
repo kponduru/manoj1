70930164c92f18c4ac92398e7a29374dc6984795
Manoj Ponduru <kponduru@aol.com>
2023-04-13 15:14:18 -0500

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,22 +1,22 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+# define the map as a list of strings
+india_map = [
+    "              ..::::::::::::::..              ",
+    "        ..:::aad888888888888baa:::..        ",
+    "     .::::d888888888888888888888b:::.     ",
+    "   .:::d8888f   88888   f8888888b:::.   ",
+    "  ::::d8888!   d88888b   !8888b8888::::  ",
+    " :::::88888   88888888   88888888::::: ",
+    ":::::88888   88888888   88888888::::::",
+    ":::::88888   88888888   88888888::::::",
+    ":::::88888   88888888   88888888::::::",
+    " :::::88888   88888888   88888888::::: ",
+    "  :::::88888   88888888   8888888::::  ",
+    "   ':::::8888!   88888   !8888P::::'   ",
+    "     ':::::d88888888888888baaa::::'     ",
+    "        '::::ad88888888888888::::'        ",
+    "           '':::bbbbbb::::::''           "
+]
 
-# create a map object with India's coordinates
-map = Basemap(llcrnrlon=68,llcrnrlat=7,urcrnrlon=89,urcrnrlat=35, resolution='l')
-
-# draw coastlines and country boundaries
-map.drawcoastlines()
-map.drawcountries()
-
-# fill the land with a light green color
-map.fillcontinents(color='lightgreen',lake_color='white')
-
-# draw the meridians and parallels
-map.drawmeridians(range(60,100,5),labels=[False,False,False,True])
-map.drawparallels(range(0,40,5),labels=[True,False,False,False])
-
-# add a title
-plt.title('Map of India')
-
-# show the map
-plt.show()
+# print the map
+for line in india_map:
+    print(line)
